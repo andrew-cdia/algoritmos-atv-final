50425743d257f45ca834e72b5dbdca4d636599c5
wllandrew <andrew.willian@uel.br>
2025-07-22 00:59:33 -0300

    feat: resolução ingenua e atual

--- 3 - Agencia de Marketing/ex.py
+++ 3 - Agencia de Marketing/ex.py
@@ -1,6 +1,10 @@
+"""
+Entrada de Dados.
+"""
+
 ORCAMENTO = int(input())
 
-inf = []
+INFLUENCIADORES = list()
 
 for _ in range(int(input())):
     linha = input().split(", ")
@@ -8,26 +12,74 @@ for _ in range(int(input())):
     custo = int(linha[0])
     seguidores = set(linha[1:])
 
-    inf.append([custo, seguidores])
+    INFLUENCIADORES.append([custo, seguidores])
 
-def ingenua(inf : list, orc : int) -> tuple:
-    pass
+"""
+Métodos.
+"""
 
-def atual(inf : list, orc : int) -> tuple:
-    pass
+def ingenua(influenciadores : list, orcamento : int) -> tuple:
+    """
+    Função que implementa o método ingênuo:
+    inf : list = lista de influenciadores, onde inf[0] é o custo e inf[1] conjunto de seguidores.
+    orc : int = orçamento total
+    """
+    custo = [influenciador[0] for influenciador in influenciadores]
+    custo = dict(enumerate(custo))
+    custo = sorted(custo.items(), key=lambda k: k[1])
+
+    gasto, seguidores = 0, set()
+
+    for index, valor in custo:
+        if gasto + valor > orcamento:
+            break
+        gasto += valor
+        seguidores |= influenciadores[index][1]
+
+    return (gasto, len(seguidores))
+    
+
+def atual(influenciadores : list, orcamento : int) -> tuple:
+    """
+    Função que implementa o método atual:
+    inf : list = lista de influenciadores, onde inf[0] é o custo e inf[1] conjunto de seguidores.
+    orc : int = orçamento total
+    """
+    custo_beneficio = [influenciador[0] / len(influenciador[1]) for influenciador in influenciadores]
+    custo_beneficio = dict(enumerate(custo_beneficio))
+    custo_beneficio = sorted(custo_beneficio.items(), key=lambda k: k[1])
+
+    gasto, seguidores = 0, set()
+
+    for index, _ in custo_beneficio:
+        if gasto + influenciadores[index][0] > orcamento:
+            break
+        gasto += influenciadores[index][0]
+        seguidores |= influenciadores[index][1]       
+    
+    return (gasto, len(seguidores))
+
 
 def nova(inf : list, orc : int) -> tuple:
     pass
 
 
-ing = ingenua(inf, ORCAMENTO)
-print(ing)
-print(f"Estratégia Ingênua: {ing[0]:.01}, {ing[1]}")
+"""
+Execução principal do programa.
+"""
 
-at = atual(inf, ORCAMENTO)
-print(at)
-print(f"Estratégia Atual: {at[0]:.01}, {at[1]}")
+if __name__ == "__main__":
+    ing = ingenua(INFLUENCIADORES, ORCAMENTO)
+    print(ing)
+    print(f"Estratégia Ingênua: {ing[0]}.0, {ing[1]}")
 
-nv = nova(inf, ORCAMENTO)
-print(nv)
-print(f"Estratégia Nova: {nv[0]:.01}, {nv[1]}")
+    
+    at = atual(INFLUENCIADORES, ORCAMENTO)
+    print(at)
+    print(f"Estratégia Atual: {at[0]}.0, {at[1]}")
+
+    """
+    nv = nova(INFLUENCIADORES, ORCAMENTO)
+    print(nv)
+    print(f"Estratégia Nova: {nv[0]}.0, {nv[1]}")
+    """
