a198497280cb5763da6f232ca5153df223906da4
wllandrew <andrew.willian@uel.br>
2025-07-22 20:15:57 -0300

    feat: comeco do exercicio 4

--- 4 - Tesouros perdidos/ex.py
+++ 4 - Tesouros perdidos/ex.py
@@ -20,10 +20,35 @@ PERIMETRO_MAXIMO = int(input())
 Funções
 """
 
+def maior_perimetro(matriz : list) -> int:
+    tesouros = []
+
+    for linha in range(DIMENSOES[0]):
+        for coluna in range(DIMENSOES[1]):
+
+            calculo, p = 0, 0
+
+            for i in range(linha, DIMENSOES[0]):
+                for j in range(coluna, DIMENSOES[1]):
+                    
+                    p = 2 * ((i - linha) + (j - coluna) + 2)
+
+                    if p > PERIMETRO_MAXIMO:
+                        break
+                    
+                    soma = matriz[linha:i]
+                    soma = [a[coluna:j] for a in soma]
+
+                    for n in soma:
+                        calculo += n.count("X")
+
+            tesouros.append(calculo)
+
+    return max(tesouros)
 
 """
 Execução principal
 """
 
 if __name__ == "__main__":
-    pass
+    print(maior_perimetro(TABULEIRO))
